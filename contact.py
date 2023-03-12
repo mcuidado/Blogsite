ee973761dd41a325e051d8047e4313e11e89d568
Matthew I. Hippensteel <michaelcuidado081@gmail.com>
2023-03-12 10:46:42 -0700

    Added working POST request/form functionality

---
+++ contact.py
@@ -0,0 +1,30 @@
+
+from smtplib import SMTP, SMTPAuthenticationError
+
+EMAIL="michaelcuidado081@gmail.com"
+APP_PASS="vjwgsezdsqaktsfz"
+
+class Contact():
+    def __init__(self, name, email, phone, message):
+        self.name = name
+        self.email = email
+        self.phone = phone
+        self.message = message
+    
+    def send_email(self):
+        with SMTP("smtp.gmail.com" ,port=587) as smtp:
+            try:
+                smtp.starttls()
+                sender = smtp.login(user=EMAIL, password=APP_PASS)
+            except SMTPAuthenticationError:
+                print(f"{SMTPAuthenticationError.smtp_error}")
+            else:
+                msg_body = f"Name: {self.name}\nEmail: {self.email}\nPhone: {self.phone}\nMessage: {self.message}"
+                msg = f"Subject: New message from {self.name}\n\n{msg_body}"
+                smtp.sendmail(
+                    from_addr=EMAIL, 
+                    to_addrs=EMAIL,
+                    msg=msg
+                )
+
+        
--- server.py
+++ server.py
@@ -1,15 +1,17 @@
-from flask import Flask, render_template, url_for, redirect
+from flask import Flask, render_template, url_for, redirect, request, make_response
 import time
 from random import randint
 from datetime import datetime
 import requests
 import json
 from blog import Blogs
+from contact import Contact
+
+# Github access token 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
 
 app = Flask(__name__)
 
 blogs = Blogs()
-
 BLOG_DATA = blogs.get_all_blog_data()
 
 FOOTER_DATA = {
@@ -35,6 +37,31 @@ def read_selected_blog(id):
 @app.route("/about")
 def about_me():
     return render_template("about.html", data=BLOG_DATA, footer=FOOTER_DATA)
+
+@app.route("/contact", methods=["POST", "GET"])
+def contact_me():
+    if request.method == "POST":
+        send_mail = Contact(
+            name=request.form["name"],
+            email=request.form["email"],
+            phone=request.form["phoneNumber"],
+            message=request.form["messageArea"]
+        )
+
+        send_mail.send_email()
+        return render_template("contact.html", data=BLOG_DATA, footer=FOOTER_DATA, form_data=request.form)
+    else:
+        userMessage = "Your data was not sent :-("
+        return render_template("contact.html", data=BLOG_DATA, footer=FOOTER_DATA, message=userMessage)
+
+# @app.route("/form-entry", methods=["POST", "GET"])
+# def formEntry():
+#     if request.method == "POST":
+#         return render_template("form-entry.html", data=BLOG_DATA, footer=FOOTER_DATA, form_data=request.form)
+#     else:
+#         userMessage = "Your data was not sent :-("
+#         return render_template("form-entry.html", data=BLOG_DATA, footer=FOOTER_DATA, message=userMessage)
+    
 
 if __name__ == "__main__":
     app.run(debug=True)
